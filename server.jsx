bec6fa07b33e9617f4dcf8339508042ba6ef0589
bigredtiger <simon@bigredtiger.com>
2018-03-11 19:56:22 -0400

    Test

--- server.jsx
+++ server.jsx
@@ -6,7 +6,7 @@ const path = require('path');
 
 const host = '127.0.0.1';
 const port = 8124;
-
+//test
 function allowCrossDomain(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
